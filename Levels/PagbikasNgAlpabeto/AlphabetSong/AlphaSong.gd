46160c566770d7ef3de10813b8c9ef3dcb022fd3
Ismael Cobble <92829860+Moggle-Khraum@users.noreply.github.com>
2023-11-21 22:22:44 +0800

    adds 3 slots
    Adds 3 Slots for 3 Videos..

--- Levels/PagbikasNgAlpabeto/AlphabetSong/AlphaSong.gd
+++ Levels/PagbikasNgAlpabeto/AlphabetSong/AlphaSong.gd
@@ -1,10 +1,8 @@
 extends Control
 
-
-
 func _ready() -> void:
-	$'%VideoPlayer'.stop()
-	print("VP is Stop")
+	$'%VPponetaka'.stop()
+	print("Ponetaka has Stop")
 
 func _on_LeftButton_pressed() -> void:
 	ScrollPop.play()
@@ -12,34 +10,46 @@ func _on_LeftButton_pressed() -> void:
 	var goLvl2 = get_tree().change_scene("res://Levels/PagbikasNgAlpabeto/NextLevelP2Z/LetterSpeak2.tscn")
 	print("Going Back")
 
-func _on_PLAYTHEVIDEO_button_down() -> void:
-	$'%VideoPlayer'.play()
-	print("Un Paused the Video")
+func _on_PlayVPonetaka_button_down() -> void:
+	$'%VPponetaka'.play()
+	print("Un Paused Ponetaka")
 	
-	if $'%VideoPlayer'.is_playing():
-		$'%PLAYTHEVIDEO'.hide()
+	if $'%VPponetaka'.is_playing():
+		$'%PlayVPonetaka'.hide()
 		$'%curtain'.hide()
-		print("VP is Playing")
-	elif !$'%VideoPlayer'.is_playing():
-		$'%PLAYTHEVIDEO'.show()
+		print("Poentaka is Playing")
+		
+	elif !$'%VPponetaka'.is_playing():
+		$'%PlayVPonetaka'.show()
 		$'%curtain'.show()
-		print("VP is Not Playing")
+		print("Ponetaka is Not Playing")
+		
 	
-func _on_VideoPlayer_finished() -> void:
+func _on_VPponetaka_finished() -> void:
 	$'%curtain'.show()
-	$'%PLAYTHEVIDEO'.show()
+	$'%PlayVPonetaka'.show()
 	print("Show Button | Curtain")
 	
-	
-#----------------------------------
+func _on_Ponetaka_pressed() -> void:
+	$'%Ponetaka'.show()
+	print("Show Ponetaka")
+	$'%thirdVideo'.hide()
+	$'%secondVideo'.hide()
+	$'%firstVideo'.hide()
+	$'%LeftButton'.hide()
+	print("Hides Left Button and 2 Videos")
 
-# Speed of the rotation
-var angular_speed = PI
-# Angular speed of one full rotation per second
+#Exit Video
+func _on_exitPonetaka_pressed() -> void:
+	$'%Ponetaka'.hide()
+	print("Hides Ponetaka")
+	$'%thirdVideo'.show()
+	$'%secondVideo'.show()
+	$'%firstVideo'.show()
+	$'%LeftButton'.show()
+	print("Shows Left Button and 2 Videos")
+	var gobacktostart = get_tree().change_scene("res://Levels/PagbikasNgAlpabeto/AlphabetSong/AlphaSong.tscn")
+	print("Restart the Scene")
 
-func _process(delta: float) -> void:
-	#Rotate the gear sprite
-		$'%cog'.rotation += angular_speed * delta
-		$'%cog0'.rotation -= angular_speed * delta
-		$'%cog1'.rotation -= angular_speed * delta
-		$'%cog2'.rotation += angular_speed * delta
+
+
