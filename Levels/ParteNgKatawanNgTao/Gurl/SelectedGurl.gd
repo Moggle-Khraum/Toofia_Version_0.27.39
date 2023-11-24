b997ad4bac691e400e7e88449f71e77069d2993f
Ismael Cobble <92829860+Moggle-Khraum@users.noreply.github.com>
2023-11-24 12:52:38 +0800

    Add DragNDrop
    Draggable and Droppable

--- Levels/ParteNgKatawanNgTao/Gurl/SelectedGurl.gd
+++ Levels/ParteNgKatawanNgTao/Gurl/SelectedGurl.gd
@@ -1,6 +1,18 @@
 extends Control
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 func _on_goBack_pressed() -> void:
 	var toGurlBoi = get_tree().change_scene("res://Levels/ParteNgKatawanNgTao/ParteNgKatawanNgTao.tscn")
 	print("Going Back")
