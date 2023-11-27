d8f616551b2115e48edf49e620737f6d795c7a2f
Ismael Cobble <92829860+Moggle-Khraum@users.noreply.github.com>
2023-11-27 23:05:21 +0800

    added drag N drop

--- Levels/ParteNgKatawanNgTao/Boi/leftArm/leftArm.gd
+++ Levels/ParteNgKatawanNgTao/Boi/leftArm/leftArm.gd
@@ -5,14 +5,20 @@ var rest_point
 var rest_nodes = []
 
 func _ready():
-	rest_nodes = get_tree().get_nodes_in_group("zone")
+	rest_nodes = get_tree().get_nodes_in_group("leftArm")
 	rest_point = rest_nodes[0].global_position
 	rest_nodes[0].select()
 
 func _on_Area2D_input_event(_viewport, _event, _shape_idx):
 	if Input.is_action_just_pressed("click"):
 		selected = true
-		
+
+func _on_Area2D_mouse_entered() -> void:
+	scale = Vector2(1.08, 1.08)
+
+func _on_Area2D_mouse_exited() -> void:
+	scale = Vector2(1, 1)
+
 func _physics_process(delta):
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
@@ -32,12 +38,3 @@ func _input(event):
 					child.select()
 					rest_point = child.global_position
 					shortest_dist = distance
-
-
-
-func _on_Area2D_mouse_entered() -> void:
-	scale = Vector2(1.08, 1.08)
-
-func _on_Area2D_mouse_exited() -> void:
-	scale = Vector2(1, 1)
-	
